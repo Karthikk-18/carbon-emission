362792cebce79f20a247480ad45f632cb973f237
Karthik Yadav <karthikgudaru72@gmail.com>
2026-02-15 11:12:57 +0530

    FIX: fixed server serving static files

--- server.js
+++ server.js
@@ -2,19 +2,28 @@ import express from 'express';
 import { calculateCarbonStats } from './cec-logic.js';
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
+//serve static files using frintend directory
+app.use(express.static('frontend'));
+
 app.post('/calculate', (req, res) => {
     try {
+
+        if(!req.body || typeof req.body !== 'object'){
+            return res.status(400).json({error : 'Invalid request Body'});
+        }
+
         const result = calculateCarbonStats(req.body);
         res.json(result);
     } catch (error) {
-        res.status(500).json({error : error.message});
+        console.log("calculation error : ",error);
+        return res.status(400).json({error: "failed to calculate emissions, Please check your Input values"});
     }
 });
 
 app.listen(port, () => {
-    console.log(`WEBSITE is running on http://localhost:${port}`);
+    console.log(`server is running on http://localhost:${port}`);
 });
